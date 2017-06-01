8e806915c037301f079b2447a4af73b9678beb26
Phil Cooper-King <phil@cooperking.net>
2017-06-01 07:27:53 +0100

    inital impl of nearest edge.

--- math/geometry/ray.hpp
+++ math/geometry/ray.hpp
@@ -20,6 +20,7 @@ inline vec3       ray_direction(const ray &ray);
 inline float      ray_test_aabb(const ray &ray, const aabb &target);
 inline bool       ray_test_plane(const ray &ray, const plane &target, float *out_distance = nullptr);
 inline bool       ray_test_triangles(const ray &in_ray, const float tris[], const size_t tri_count, float *out_distance = nullptr);
+inline bool       ray_test_closest_edge(const float tris[], const size_t tri_count, const vec3 point, vec3 &seg_a, vec3 &seg_b);
 
 
 // ---------------------------------------------------------------- [ Impl ] --
@@ -125,19 +126,26 @@ ray_test_triangles(
   const size_t tri_count,
   float *out_distance)
 {
-  const vec3 r_dir = ray_direction(in_ray);
+  const vec3 r_dir = MATH_NS_NAME::ray_direction(in_ray);
   
   for(size_t i = 0; i < tri_count; ++i)
   {
-    const size_t tri = i * 3;
     const size_t tri_index = i * 3 * 3;
    
-    const vec3 v0 = vec3_init_with_array(&tris[tri_index]);
-    const vec3 v1 = vec3_subtract(vec3_init_with_array(&tris[tri_index + 3]), v0);
-    const vec3 v2 = vec3_subtract(vec3_init_with_array(&tris[tri_index + 6]), v0);
+    const vec3 v0 = MATH_NS_NAME::vec3_init_with_array(
+      &tris[tri_index]
+    );
+    
+    const vec3 v1 = MATH_NS_NAME::vec3_subtract(
+      MATH_NS_NAME::vec3_init_with_array(&tris[tri_index + 3]), v0
+    );
+    
+    const vec3 v2 = MATH_NS_NAME::vec3_subtract(
+      MATH_NS_NAME::vec3_init_with_array(&tris[tri_index + 6]), v0
+    );
 
-    const vec3 p_vec = vec3_cross(r_dir, v2);
-    const float dot  = vec3_dot(v1, p_vec);
+    const vec3 p_vec = MATH_NS_NAME::vec3_cross(r_dir, v2);
+    const float dot  = MATH_NS_NAME::vec3_dot(v1, p_vec);
 
     if(dot < MATH_NS_NAME::epsilon())
     {
@@ -145,23 +153,23 @@ ray_test_triangles(
     }
 
     const float o_dot = 1 / dot;
-    const vec3 t_vec  = vec3_subtract(in_ray.start, v0);
-    const float u     = vec3_dot(t_vec, p_vec) * o_dot;
+    const vec3 t_vec  = MATH_NS_NAME::vec3_subtract(in_ray.start, v0);
+    const float u     = MATH_NS_NAME::vec3_dot(t_vec, p_vec) * o_dot;
   
     if(!MATH_NS_NAME::is_between(u, 0.f, 1.f))
     {
       continue;
     }
  
-    const vec3 q_vec = vec3_cross(t_vec, v1);
-    const float v    = vec3_dot(r_dir, q_vec) * o_dot;
+    const vec3 q_vec = MATH_NS_NAME::vec3_cross(t_vec, v1);
+    const float v    = MATH_NS_NAME::vec3_dot(r_dir, q_vec) * o_dot;
   
     if (v < 0 || u + v > 1)
     {
       continue;
     }
  
-    const float t = vec3_dot(v2, q_vec) * o_dot;
+    const float t = MATH_NS_NAME::vec3_dot(v2, q_vec) * o_dot;
   
     if(out_distance)
     {
@@ -175,6 +183,84 @@ ray_test_triangles(
   return false;
 }
 
+
+bool
+ray_test_closest_edge(const float tris[], const size_t tri_count, const vec3 point, vec3 &seg_a, vec3 &seg_b)
+{
+  constexpr float max_dist = 10000000000.0;
+  
+  float curr_closest = max_dist;
+  
+  vec3 curr_segment[2];
+  
+  for(size_t i = 0; i < tri_count; ++i)
+  {
+    const size_t tri_index = i * 3 * 3;
+    
+    const vec3 verts[] {
+      vec3_init_with_array(&tris[tri_index + 0]),
+      vec3_init_with_array(&tris[tri_index + 3]),
+      vec3_init_with_array(&tris[tri_index + 6]),
+    };
+    
+    // Segments
+    for(uint32_t j = 0; j < 3; ++j)
+    {
+      const size_t va = j;
+      const size_t vb = (j + 1) % 3;
+    
+      vec3 v = vec3_subtract(verts[vb], verts[va]);
+      vec3 w = vec3_subtract(point, verts[va]);
+      
+      float c1 = vec3_dot(w, v);
+      
+      if(c1 <= 0)
+      {
+        const float dist = vec3_length(vec3_subtract(point, verts[va]));
+        
+        if(dist < curr_closest)
+        {
+          curr_closest = dist;
+          curr_segment[0] = verts[va];
+          curr_segment[1] = verts[vb];
+        }
+        continue;
+      }
+      
+      float c2 = vec3_dot(v, v);
+      if(c2 <= c1)
+      {
+        const float dist = vec3_length(vec3_subtract(point, verts[vb]));
+        
+        if(dist < curr_closest)
+        {
+          curr_closest = dist;
+          curr_segment[0] = verts[va];
+          curr_segment[1] = verts[vb];
+        }
+        continue;
+      }
+      
+      float b = c1 / c2;
+      
+      vec3 p = vec3_add(verts[va], vec3_scale(v, b));
+      
+      const float dist = vec3_length(vec3_subtract(point, p));
+      
+      if(dist < curr_closest)
+      {
+        curr_closest = dist;
+        curr_segment[0] = verts[va];
+        curr_segment[1] = verts[vb];
+      }
+    }
+  }
+  
+  seg_a = curr_segment[0];
+  seg_b = curr_segment[1];
+  
+  return curr_closest < max_dist;
+}
 
 
 _MATH_NS_CLOSE
